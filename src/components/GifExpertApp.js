2e2486bcfdab927cad880a6d2dd1e806ea5b93f8
Pedro Calvo Quesada <pedrocalvoquesada@gmail.com>
2021-06-12 14:52:03 +0200

    Listado de categorias

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const GifExpertApp = () => {
+
+   // const categories = [ 'One Punch', 'Samurai X', 'Dragon Ball' ];
+
+   const [categories, setcategories] = useState( [ 'One Punch', 'Samurai X', 'Dragon Ball' ] );
+
+   const handleAdd = () => {
+        setcategories( [...categories, 'HunterxHunter'] );
+   }
 
     return (
 
@@ -8,6 +16,12 @@ export const GifExpertApp = () => {
             <h2>GifExpertApp</h2>
 
             <hr />
+
+            <button onClick={ handleAdd }>Agregar categoría</button>
+
+            <ol>
+                { categories.map( category => <li key ={ category }>{ category }</li> ) }
+            </ol>
         </>
 
     );
